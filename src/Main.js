b6a8070cdcb81157ad23e98d80d731948b6fae25
obaldan <taviu522@gmail.com>
2019-01-03 16:32:22 -0200

    adding some pages and custom menu

---
+++ src/Main.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {
+    Route,
+    NavLink,
+    HashRouter
+  } from "react-router-dom";
+
+import Home from './pages/home';
+import Contact from './pages/contact';
+import Stuff from './pages/stuff';
+import About from './pages/about';
+import './styles.css';
+
+
+class Main extends Component{
+    render() {
+        return (
+            <HashRouter>
+                <div>
+                    
+                    <div className="menu">
+                        <h1 className="menuLogo">Simple List</h1>
+                        <div className="menuRight">
+                            <ul className="header">
+                                <li><NavLink exact to="/" activeClassName="activeNav">Home</NavLink></li>
+                                <li><NavLink exact to="/contact" activeClassName="activeNav">Contact</NavLink></li>
+                                <li><NavLink exact to="/stuff" activeClassName="activeNav">Stuff</NavLink></li>
+                                <li><NavLink exact to="/about" activeClassName="activeNav">Sobre a empresa</NavLink></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="content">
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/contact" component={Contact} />
+                        <Route exact path="/stuff" component={Stuff} />
+                        <Route exact path="/about" component={About} />
+                    </div>
+                </div>
+            </HashRouter>
+            
+        );
+    }
+
+};
+
+export default Main;
---
+++ src/pages/about/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class About extends Component {
+    render() {
+        return(
+            <div>
+                <h2>Sobre a empresa</h2>
+
+                <p>Há 25 anos um de nossos diretores começou a consertar os brinquedos de seus filhos e percebeu sua enorme habilidade e prazer com os brinquedos.</p>
+                
+                <p>Parentes e vizinhos começaram a entregar os brinquedos para serem consertados, sempre com ótimos resultados. 
+                    A idéia estava lançada: <b>Por que não fazer um Pronto Socorro das Bonecas?</b></p>
+                
+                <p>Seus fundadores começaram a aperfeiçoar-se fazendo cursos nas principais indústrias de brinquedos, entre elas: Estrela, Tec-Toy e outras importantes marcas. 
+                    Hoje, graças a sua equipe técnica de profissionais altamente especializados, o PS das Bonecas é posto autorizado das principais indústrias de brinquedos.</p>
+                
+                <p>Com o carinho e a experiência de quem já devolveu a alegria há muitas crianças com seus brinquedos funcionando como novos, aqui você pode restaurar bonecas novas e antigas, consertar video-games e muitos outros brinquedos nacionais e importados.</p>
+                
+            </div>
+        );
+    }
+}
